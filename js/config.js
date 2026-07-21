e708227241f9caac4529aafa675109ff162dbee9
iraqi_eco <0m7q7k@gmail.com>
2026-07-21 19:58:20 +0300

    Update Supabase configuration with URL and anonKey

--- js/config.js
+++ js/config.js
@@ -65,9 +65,9 @@ export const CONFIG = Object.freeze({
 
     supabase: {
 
-        url: "",
+        url: "https://rmqtopxawrrgntcqqnpa.supabase.co",
 
-        anonKey: ""
+        anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtcXRvcHhhd3JyZ250Y3FxbnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3OTIxMzQsImV4cCI6MjA5ODM2ODEzNH0.HoExHY9oNFdryWUC2u2UELLPfBNhH3P4L3zS03dvELA"
 
     },
 
